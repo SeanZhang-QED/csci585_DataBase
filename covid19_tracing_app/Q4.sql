cb4869b616851525246c09bed35087fba9c5e3f7
Xuan <66594541+SeanZhang-QED@users.noreply.github.com>
2021-10-13 23:03:34 -0700

    update Q4, adding a different way of solution

--- covid19_tracing_app/Q4.sql
+++ covid19_tracing_app/Q4.sql
@@ -2,6 +2,7 @@
 
 -- the given period is from 
 -- 2021-10-1 to 2021-10-15 
+-- The number of employees implies the multiple reports from a specific employee will only be counted once. 
 SELECT SUM(c1) AS Num_of_Scan, SUM(c2) AS Num_of_Test, SUM(c3) AS Num_of_Positive, SUM(c4) AS Num_of_Reported_Emp FROM (
 	SELECT COUNT(Scan_ID) c1, 0 c2, 0 c3, 0 c4 FROM scan WHERE Scan_Date BETWEEN '2021-10-1' AND '2021-10-15'
 	UNION ALL
@@ -12,9 +13,10 @@ SELECT SUM(c1) AS Num_of_Scan, SUM(c2) AS Num_of_Test, SUM(c3) AS Num_of_Positiv
 	SELECT 0 c1, 0 c2, 0 c3, COUNT(DISTINCT EMP_ID) c4 FROM symptom WHERE Date_Reported BETWEEN '2021-10-1' AND '2021-10-15'
 ) d;
 
--- 
--- SELECT COUNT(DISTINCT Scan_ID), COUNT(DISTINCT Test_ID), COUNT(DISTINCT symptom.EMP_ID), COUNT(Test_result = 'positive')
--- FROM scan, test, symptom, 
--- WHERE (Scan_Date BETWEEN '2021-10-1' AND '2021-10-15') 
--- AND (Test_Date BETWEEN '2021-10-1' AND '2021-10-15')
--- AND (Date_Reported BETWEEN '2021-10-1' AND '2021-10-15');
+-- An easier way
+SELECT *
+FROM
+(SELECT COUNT(Scan_ID) AS Num_of_Scan FROM scan WHERE Scan_Date BETWEEN '2021-10-1' AND '2021-10-15') d1,
+(SELECT COUNT(Test_ID) AS Num_of_Test FROM test WHERE Test_Date BETWEEN '2021-10-1' AND '2021-10-15') d2,
+(SELECT COUNT(*) AS Num_of_Positive FROM test WHERE (Test_Date BETWEEN '2021-10-1' AND '2021-10-15') AND Test_result = 'positive') d3,
+(SELECT COUNT(DISTINCT EMP_ID) AS Num_of_Reported_Emp FROM symptom WHERE Date_Reported BETWEEN '2021-10-1' AND '2021-10-15') d4;
